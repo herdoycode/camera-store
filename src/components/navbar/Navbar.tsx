d870b8a3eaefd3f6a260836f9b8b576c7e56895f
= <=>
2023-07-23 23:30:56 +0600

    fix navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -22,59 +22,62 @@ const Navbar = () => {
   }, [isCollapse]);
 
   return (
-    <div className="navbar">
-      <div className="left">
-        <div onClick={handleNavToggle} className="toggler">
-          {isCollapse ? (
-            <FiMenu className="icon" />
-          ) : (
-            <MdClose className="icon" />
-          )}
+    <>
+      <div className="navbar">
+        <div className="left">
+          <div onClick={handleNavToggle} className="toggler">
+            {isCollapse ? (
+              <FiMenu className="icon" />
+            ) : (
+              <MdClose className="icon" />
+            )}
+          </div>
+          <Link to="/" className="logo">
+            EStore
+          </Link>
+          <div className="serarch">
+            <input type="text" placeholder="Search products..." />
+            <BiSearchAlt2 className="icon" />
+          </div>
         </div>
-        <Link to="/" className="logo">
-          EStore
-        </Link>
-        <div className="serarch">
-          <input type="text" placeholder="Search products..." />
-          <BiSearchAlt2 className="icon" />
+        <div className={navClass}>
+          <ul>
+            <li>
+              <Link onClick={handleNavToggle} className="link" to="/products">
+                Products
+              </Link>
+            </li>
+            <li>
+              <Link onClick={handleNavToggle} className="link" to="/login">
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link onClick={handleNavToggle} className="link" to="/signup">
+                Signup
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="right">
+          <div className="item">
+            <img src="https://i.ibb.co/CJzdzdc/me.jpg" alt="user" />
+          </div>
+          <div onClick={toggleTheme} className="item">
+            {mode === "dark" ? (
+              <BsSun className="icon" />
+            ) : (
+              <BsMoon className="icon" />
+            )}
+          </div>
+          <div className="item">
+            <MdOutlineShoppingCart className="icon" />
+            <div className="count">0</div>
+          </div>
         </div>
       </div>
-      <div className={navClass}>
-        <ul>
-          <li>
-            <Link onClick={handleNavToggle} className="link" to="/products">
-              Products
-            </Link>
-          </li>
-          <li>
-            <Link onClick={handleNavToggle} className="link" to="/login">
-              Login
-            </Link>
-          </li>
-          <li>
-            <Link onClick={handleNavToggle} className="link" to="/signup">
-              Signup
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <div className="right">
-        <div className="item">
-          <img src="https://i.ibb.co/CJzdzdc/me.jpg" alt="user" />
-        </div>
-        <div onClick={toggleTheme} className="item">
-          {mode === "dark" ? (
-            <BsSun className="icon" />
-          ) : (
-            <BsMoon className="icon" />
-          )}
-        </div>
-        <div className="item">
-          <MdOutlineShoppingCart className="icon" />
-          <div className="count">0</div>
-        </div>
-      </div>
-    </div>
+      <div className="nav-fix"></div>
+    </>
   );
 };
 
