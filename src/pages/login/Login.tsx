7879d40f97763585d4105bbe4c4b89c3bb447b81
= <=>
2023-08-03 21:52:50 +0600

    Create login and signup page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
 import "./Login.scss";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div className="box">
+        <h2>Login</h2>
+        <form>
+          <input type="text" placeholder="Email address...." />
+          <input type="text" placeholder="Password...." />
+          <button type="submit">Login</button>
+        </form>
+        <Link className="link" to="/signup">
+          Create new account
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
 import "./Signup.scss";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div className="signup">
+      <div className="box">
+        <h2>Signup</h2>
+        <form>
+          <input type="text" placeholder="Full Name..." />
+          <input type="text" placeholder="Email address...." />
+          <input type="text" placeholder="Password...." />
+          <button type="submit">Submit</button>
+        </form>
+        <Link className="link" to="/login">
+          Already have account? login
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
